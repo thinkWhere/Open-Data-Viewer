8ee0dcfcf8119045dd06648595d18b3a03eaca5a
Martin Clarke <martin.clarke@thinkwhere.com>
2019-03-11 15:54:03 +0000

    Simplify mapThemesModal component  - change to function component

--- src/components/mapThemesModal/mapThemesModal.js
+++ src/components/mapThemesModal/mapThemesModal.js
@@ -1,46 +1,42 @@
 import React from 'react';
 import './mapThemesModal.css';
 
-export default class MapThemesModal extends React.Component {
+export default function MapThemesModal(props) {
 
-    render() {
-        let themeInformation = null;
-        if (this.props.themes) {
-            themeInformation = this.props.themes.map((theme) =>
-                <div className="media" key={`mapThemesModal + ${this.props.themes.indexOf(theme)}`}>
-                    <div className="icon-container">
-                        <span className={`fa fa-2x fa-${theme.mapConfig.mapIcon} align-self-start`}></span>
-                    </div>
-                    <div className="media-body">
-                        <h5 className="font-weight-bold">{theme.Title}</h5>
-                        <p>{theme.Description}</p>
-                    </div>
+    const themeInfo = (
+        props.themes && props.themes.map((theme) =>
+            <div className="media" key={`mapThemesModal + ${props.themes.indexOf(theme)}`}>
+                <div className="icon-container">
+                    <span className={`fa fa-2x fa-${theme.mapConfig.mapIcon} align-self-start`}></span>
                 </div>
-            )
-        }
+                <div className="media-body">
+                    <h5 className="font-weight-bold">{theme.Title}</h5>
+                    <p>{theme.Description}</p>
+                </div>
+            </div>
+        )
+    );
 
-        const element = (
-            <div className="modal fade" id="themeModal" tabIndex="-1" role="dialog"
-                 aria-labelledby="modalTitle" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title font-weight-bold" id="modalTitle">Map Themes</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            {themeInformation}
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-primary" data-dismiss="modal">Close</button>
-                        </div>
+    const themeInfoElement = (
+        <div className="modal fade" id="themeModal" tabIndex="-1" role="dialog"
+             aria-labelledby="modalTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title font-weight-bold" id="modalTitle">Map Themes</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        {themeInfo}
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-primary" data-dismiss="modal">Close</button>
                     </div>
                 </div>
             </div>
-        );
-        return element;
-    }
+        </div>
+    );
+    return themeInfoElement;
 }
-
